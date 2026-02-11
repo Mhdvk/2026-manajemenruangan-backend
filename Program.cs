d8e38d4288683fcb69fb356d56bdb9a2122bf968
Mhdvk <muhammadmahdavikia78@gmail.com>
2026-02-11 22:07:16 +0700

    Perbaikan CORS di Program.cs

--- Program.cs
+++ Program.cs
@@ -3,17 +3,29 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Tambahkan DB Context
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite("Data Source=manajemenruangan.db")
 );
 
-builder.Services.AddControllers();
+// Tambahkan CORS sebelum build
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors();
 
 if (app.Environment.IsDevelopment())
 {
