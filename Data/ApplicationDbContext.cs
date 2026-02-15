2dcedfa934f55a4705def05953d2c57c2ed40619
Mhdvk <muhammadmahdavikia78@gmail.com>
2026-02-15 10:18:22 +0700

    update seeder room pada database

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -36,119 +36,119 @@ protected override void OnModelCreating(ModelBuilder modelBuilder)
             Id = 2,
             Name = "Lab Database",
             Location = "C105",
-            Capacity = 25
+            Capacity = 30
         },
         new Room
         {
             Id = 3,
             Name = "Lab Agile",
             Location = "C206",
-            Capacity = 15
+            Capacity = 30
         },
         new Room
         {
             Id = 4,
             Name = "Ruangan Kelas A201",
             Location = "A201",
-            Capacity = 15
+            Capacity = 60
         },
         new Room
         {
             Id = 5,
             Name = "Ruangan Kelas A202",
             Location = "A202",
-            Capacity = 15
+            Capacity = 60
         },
         new Room
         {
             Id = 6,
             Name = "Ruangan Kelas A203",
             Location = "A203",
-            Capacity = 15
+            Capacity = 60
         },
         new Room
         {
             Id = 7,
             Name = "Ruangan Kelas A204",
             Location = "A204",
-            Capacity = 15
+            Capacity = 60
         },
         new Room
         {
             Id = 8,
             Name = "Ruangan Kelas A205",
             Location = "A205",
-            Capacity = 15
+            Capacity = 60
         },
         new Room
         {
             Id = 9,
             Name = "Ruangan Kelas A301",
             Location = "A301",
-            Capacity = 15
+            Capacity = 75
         },
         new Room
         {
             Id = 10,
             Name = "Ruangan Kelas A302",
             Location = "A302",
-            Capacity = 15
+            Capacity = 75
         },
         new Room
         {
             Id = 11,
             Name = "Ruangan Kelas A303",
             Location = "A303",
-            Capacity = 15
+            Capacity = 60
         },
         new Room
         {
             Id = 12,
             Name = "Ruangan Kelas A304",
             Location = "A304",
-            Capacity = 15
+            Capacity = 60
         },
         new Room
         {
             Id = 13,
             Name = "Ruangan Kelas A305",
             Location = "A305",
-            Capacity = 15
+            Capacity = 60
         },
         new Room
         {
             Id = 14,
             Name = "Student Center",
             Location = "Gedung Student Center",
-            Capacity = 15
+            Capacity = 400
         },
         new Room
         {
             Id = 15,
             Name = "Masjid An-Nahl",
             Location = "Masjid An Nahl",
-            Capacity = 15
+            Capacity = 200
         },
                 new Room
         {
             Id = 16,
             Name = "Lapangan Merah",
             Location = "Lapangan Merah",
-            Capacity = 15
+            Capacity = 300
         },
                 new Room
         {
             Id = 17,
             Name = "Perpustakaan D3",
             Location = "Gedung D3",
-            Capacity = 15
+            Capacity = 60
         },
                 new Room
         {
             Id = 18,
             Name = "Perpustakaan Pasca Sarjana",
             Location = "Gedung Pasca Sarjana",
-            Capacity = 15
+            Capacity = 60
         }
     );
 }
---
+++ Migrations/20260215031715_UpdateRoomSeedData.cs
@@ -0,0 +1,256 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _2026_manajemenruangan_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateRoomSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Capacity",
+                value: 30);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Capacity",
+                value: 30);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Capacity",
+                value: 60);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Capacity",
+                value: 60);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Capacity",
+                value: 60);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Capacity",
+                value: 60);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Capacity",
+                value: 60);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Capacity",
+                value: 75);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Capacity",
+                value: 75);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "Capacity",
+                value: 60);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "Capacity",
+                value: 60);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "Capacity",
+                value: 60);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "Capacity",
+                value: 400);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "Capacity",
+                value: 200);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "Capacity",
+                value: 300);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "Capacity",
+                value: 60);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "Capacity",
+                value: 60);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Capacity",
+                value: 25);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Capacity",
+                value: 15);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Capacity",
+                value: 15);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Capacity",
+                value: 15);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Capacity",
+                value: 15);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Capacity",
+                value: 15);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Capacity",
+                value: 15);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Capacity",
+                value: 15);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Capacity",
+                value: 15);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "Capacity",
+                value: 15);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "Capacity",
+                value: 15);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "Capacity",
+                value: 15);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "Capacity",
+                value: 15);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "Capacity",
+                value: 15);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "Capacity",
+                value: 15);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "Capacity",
+                value: 15);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "Capacity",
+                value: 15);
+        }
+    }
+}
